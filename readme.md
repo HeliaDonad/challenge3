cfe9821928eda76eb7092fc46992de661831b606
HeliaDonad <r0936750@student.thomasmore.be>
2024-10-16 14:27:28 +0200

    commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+mongodb.com:
+username: donadellohelia
+password: iMsTd9GigKBHppy6
+
+mongodb+srv://donadellohelia:iMsTd9GigKBHppy6@cluster0.yar4h.mongodb.net/
---
+++ routes/api/v1/messages.js
@@ -0,0 +1,25 @@
+// Add routes 
+const express = require("express"); 
+const router = express.Router(); 
+const messageController = require("../../../controllers/api/v1/message.js");
+
+// GET /api/v1/messages
+
+router.get("/", messageController.index); 
+            
+// GET /api/v1/messages/:id 
+
+router.get("/:id", messageController.show); 
+    
+// POST /api/v1/messages 
+
+router.post("/", messageController.create); 
+
+// PUT /api/v1/messages/:id 
+
+router.put("/:id", messageController.update); 
+    
+// DELETE /api/v1/messages/:id 
+router.delete("/:id", messageController.destroy); 
+
+module.exports = router;
